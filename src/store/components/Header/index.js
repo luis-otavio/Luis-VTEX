0f71654a8fb14496dcc5494108596d7a58b3f75e
Luis Otavio <luisotavio05@gmail.com>
2020-05-09 16:37:29 -0300

    first commit

---
+++ src/store/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render } from "react-dom";
+
+import "./index.scss";
+import { Menu } from "./components/Menu";
+import { MiniCart } from "./components/Minicart";
+
+export const Header = () => {
+
+  $("#mi-cart").click(function() {
+    $(".carrinho-itens").addClass("showMiniCart")
+  });
+
+  $("#mi-cart").click(function () {
+    $("#backdrop-minicart").addClass("showBackdrop");
+  });
+
+  $("#backdrop-minicart").click(function () {
+    $(".carrinho-itens").removeClass("showMiniCart");
+  });
+
+  $("#backdrop-minicart").click(function () {
+    $("#backdrop-minicart").removeClass("showBackdrop");
+  });
+
+};
---
+++ src/store/components/Home/index.js
@@ -0,0 +1,9 @@
+import "./index.scss";
+import { MainBanner } from "./components/MainBanner";
+
+const Home = () => {
+
+  
+};
+
+export default Home;

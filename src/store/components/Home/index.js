837740add1278e78f1a5d4f99d3377653304866a
Luis Otavio <luisotavio05@gmail.com>
2020-05-11 11:48:16 -0300

    changes

--- src/store/components/Home/index.js
+++ src/store/components/Home/index.js
@@ -1,9 +1,23 @@
 import "./index.scss";
 import { MainBanner } from "./components/MainBanner";
 
-const Home = () => {
+export const Home = () => {
 
+  var data = null;
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("GET", "https://apiexamples.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/skuId");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.setRequestHeader("accept", "application/json");
+
+  xhr.send(data);
   
 };
 
-export default Home;
